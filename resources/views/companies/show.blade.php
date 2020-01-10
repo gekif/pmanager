20a39fe4f6f1e0cffd303641037b5ca2cdfc19f1
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-10 20:57:47 +0700

    4. Creating controllers and views : 9. How to add side menu in laravel

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -1,24 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Jumbotron -->
-    <div class="jumbotron">
-        <h1>{{ $company->name }}</h1>
-        <p class="lead">{{ $company->description }}</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
-    </div>
 
-    <!-- Example row of columns -->
-    <div class="row">
-        @foreach ($company->projects as $project)
-        <div class="col-lg-4">
-            <h2>{{ $project->name }}</h2>
-            <p class="text-danger">{{ $project->description }}</p>
-            <p><a class="btn btn-primary" href="/projects/{{ $project->id }}" role="button">View Project »</a></p>
+    <div class="col-md-9 col-lg-9 col-sm-3 pull-left">
+
+        <!-- Jumbotron -->
+        <div class="jumbotron">
+            <h1>{{ $company->name }}</h1>
+            <p class="lead">{{ $company->description }}</p>
+            <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
         </div>
-        @endforeach
+
+        <!-- Example row of columns -->
+        <div class="row" style="background: white">
+            @foreach ($company->projects as $project)
+                <div class="col-lg-4">
+                    <h2>{{ $project->name }}</h2>
+                    <p class="text-danger">{{ $project->description }}</p>
+                    <p><a class="btn btn-primary" href="/projects/{{ $project->id }}" role="button">View Project »</a></p>
+                </div>
+            @endforeach
+        </div>
+
     </div>
 
+
+    <div class="col-sm-3 colmd-3 col-lg-3 pull-right">
+
+        <div class="sidebar-module">
+            <h4>Elsewhere</h4>
+            <ol class="list-unstyled">
+                <li><a href="#">Edit</a></li>
+                <li><a href="#">Delete</a></li>
+                <li><a href="#">Add New Member</a></li>
+            </ol>
+        </div>
+
+        <div class="sidebar-module">
+            <h4>Members</h4>
+            <ol class="list-unstyled">
+                <li><a href="#">March 2014</a></li>
+            </ol>
+        </div>
+    </div>
 
 @endsection
 
