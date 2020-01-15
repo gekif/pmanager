97c085eaff25922628238f52af98204c90d5af66
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-15 17:27:15 +0700

    6. Advanced CRUD in Laravel : 5. how to save data in database in laravel

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CompaniesController extends Controller
 {
@@ -37,6 +38,21 @@ class CompaniesController extends Controller
      */
     public function store(Request $request)
     {
+        if(Auth::check()){
+
+            $company = Company::create([
+                'name' => $request->input('name'),
+                'description' => $request->input('description'),
+                'user_id' => Auth::user()->id
+            ]);
+
+            if($company){
+                return redirect()->route('companies.show', ['company'=> $company->id])
+                    ->with('success' , 'Company created successfully');
+            }
+        }
+
+        return back()->withInput()->with('errors', 'Error creating new company');
 
     }
 
