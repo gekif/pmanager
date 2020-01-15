c347ba10f6eed8e0b800c40803ec6f2de83a5abf
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-15 17:33:18 +0700

    6. Advanced CRUD in Laravel : 6. How to add more items to navigation menu

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('companies', 'CompaniesController');
 Route::resource('projects', 'ProjectsController');
 Route::resource('roles', 'RolesController');
-Route::resource('task', 'TasksController');
+Route::resource('tasks', 'TasksController');
 Route::resource('users', 'UsersController');
 Route::resource('comments', 'CommentsController');
