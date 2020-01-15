c50085eb2c6290df37494159bad6e4639c64333a
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-15 17:52:22 +0700

    6. Advanced CRUD in Laravel : 8. How to pass optional parameters in laravel

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class CompaniesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         if (Auth::check()) {
@@ -25,22 +20,13 @@ class CompaniesController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         return view('companies.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         if(Auth::check()){
@@ -61,12 +47,7 @@ class CompaniesController extends Controller
 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Company  $company
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Company $company)
     {
         $company = Company::find($company->id);
@@ -74,12 +55,7 @@ class CompaniesController extends Controller
         return view('companies.show', compact('company'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Company  $company
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Company $company)
     {
         $company = Company::find($company->id);
@@ -87,13 +63,7 @@ class CompaniesController extends Controller
         return view('companies.edit', compact('company'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Company  $company
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Company $company)
     {
         // Save Data
@@ -112,12 +82,7 @@ class CompaniesController extends Controller
         return back()->withInput();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Company  $company
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Company $company)
     {
         $findCompany = Company::find( $company->id);
@@ -131,4 +96,5 @@ class CompaniesController extends Controller
 
         return back()->withInput()->with('error' , 'Company could not be deleted');
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,11 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('companies', 'CompaniesController');
+
+Route::get('projects/create/{id}', 'ProjectsController@create');
+
 Route::resource('projects', 'ProjectsController');
+
 Route::resource('roles', 'RolesController');
 Route::resource('tasks', 'TasksController');
 Route::resource('users', 'UsersController');
